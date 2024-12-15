d74ba99c83b55f81bed55d17b6524cd79e4f2b8c
dhimashf <muhammaddhimas1410@gmail.com>
2024-12-15 15:08:07 +0700

    fix cors

--- src/routes/akunRoutes.ts
+++ src/routes/akunRoutes.ts
@@ -1,22 +1,13 @@
 import { Router } from 'express';
 import AkunController from '../controllers/akunController';
 
-const routes = Router();
-const akunController = new AkunController();
+const akunRoutes = Router();
+const controller = new AkunController();
 
-// Route for getting all accounts (Protected Route)
-routes.get('/', (req, res) => akunController.getAkun(req, res));
+akunRoutes.get('/',controller.getAkun.bind(controller)); 
+akunRoutes.get('/:no_hp',controller.getAkunByPhone.bind(controller)); 
+akunRoutes.post('/register', controller.register.bind(controller));
+akunRoutes.post('/login', controller.login.bind(controller));
+akunRoutes.post('/forgot-password', controller.forgotPassword.bind(controller));
 
-// Route for getting an account by phone number (Protected Route)
-routes.get('/:no_hp', (req, res) => akunController.getAkunByPhone(req, res));
-
-// Route for user registration (No authentication needed)
-routes.post('/register', (req, res) => akunController.register(req, res));
-
-// Route for user login (No authentication needed)
-routes.post('/login', (req, res) => akunController.login(req, res));
-
-// Route for forgot password (No authentication needed)
-routes.post('/forgot-password', (req, res) => akunController.forgotPassword(req, res));
-
-export default routes;
+export default akunRoutes;
