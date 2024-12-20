8b818280ec2fa71dffd1d9d11be08818f026eec4
dhimashf <muhammaddhimas1410@gmail.com>
2024-12-20 20:19:51 +0700

    by nik

--- src/routes/biodataRoutes.ts
+++ src/routes/biodataRoutes.ts
@@ -7,7 +7,7 @@ const controller = new BiodataController();
 
 biodataRoutes.get('/', controller.GetAllBiodata.bind(controller));
 biodataRoutes.get('/:akun_id_akun', controller.getBiodataById.bind(controller));
-biodataRoutes.get('nik/:nik', controller.getBiodataByNik.bind(controller));
+biodataRoutes.get('/nik/:nik', controller.getBiodataByNik.bind(controller));
 biodataRoutes.post('/', upload.single('foto_ktp'), controller.addBiodata.bind(controller));
 biodataRoutes.put('/:nik', upload.single('foto_ktp'), controller.updateBiodata.bind(controller));
 
