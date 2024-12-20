f928f8b74ff5b2f0ad14efafde08f54bf70a8ba6
dhimashf <muhammaddhimas1410@gmail.com>
2024-12-20 19:29:12 +0700

    penyewaan by nik

--- src/services/penyewaanService.ts
+++ src/services/penyewaanService.ts
@@ -52,17 +52,6 @@ class PenyewaanService {
         const [result]: any = await this.db.query('DELETE FROM penyewaan WHERE id_sewa = ?', [id_sewa]);
         return result.affectedRows > 0;
     }
-     public async getPermintaanPenyewaan(biodata_nik: string): Promise<RowDataPacket[]> {
-        const [rows] = await this.db.query<RowDataPacket[]>(
-            `SELECT b.nik, b.nama, b.alamat, b.jenis_kelamin, b.alamat_domisili, b.foto_ktp, 
-            p.lokasi AS lokasi_booth, p.durasi
-            FROM biodata b
-            JOIN penyewaan p ON b.nik = p.biodata_nik
-            WHERE b.nik = ?`,
-            [biodata_nik]
-        );
-        return rows;
-    }
 }
 
 
