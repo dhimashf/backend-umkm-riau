43950ee77bbd1607f32a7ebbb33d9384ce9e8695
dhimashf <muhammaddhimas1410@gmail.com>
2024-12-26 01:23:42 +0700

    fix it

--- src/controllers/bayarsewaController.ts
+++ src/controllers/bayarsewaController.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from 'express';
 import BayarSewaService from '../services/bayarsewaService';
-
 class bayarSewaController {
     private BayarSewaService = new BayarSewaService();
 
