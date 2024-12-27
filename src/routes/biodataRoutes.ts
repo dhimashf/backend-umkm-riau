7c662c5ccc8a3e80fc16bf068ea4818bd2522276
dhimashf <muhammaddhimas1410@gmail.com>
2024-12-27 11:32:23 +0700

    no auth

--- src/routes/biodataRoutes.ts
+++ src/routes/biodataRoutes.ts
@@ -7,7 +7,7 @@ const biodataRoutes = Router();
 const controller = new BiodataController();
 const auth = new AuthMiddleware();
 
-biodataRoutes.get('/', auth.verifyToken,auth.checkRole('KEPALA DIVISI'), controller.GetAllBiodata.bind(controller));
+biodataRoutes.get('/', controller.GetAllBiodata.bind(controller));
 biodataRoutes.get('/:akun_id_akun', controller.getBiodataById.bind(controller));
 biodataRoutes.get('/nik/:nik', controller.getBiodataByNik.bind(controller));
 biodataRoutes.post('/', upload.single('foto_ktp'), controller.addBiodata.bind(controller));
